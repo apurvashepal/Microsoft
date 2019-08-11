8fac4ac7a120b410c49a68e6984e8c4d009e674a
apurvashepal <shepalag28@gmail.com>
2019-08-12 00:18:29 +0530

    added entityframerwork

---
+++ MvcApplication1/MvcApplication1/Controllers/CheckInController.cs
@@ -0,0 +1,210 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class CheckInController : Controller
+    {
+        //
+        // GET: /CheckIn/
+
+        public RoomEntities db = new RoomEntities();
+        static List<CheckIn> Guestdetails = new List<CheckIn>();
+        static  Room rm = new Room();
+          //int quantity =  //Available Room
+        // int CheckinId = 1000;
+         int stNum = RoomController.StNum;
+         int prNum = RoomController.PrNum;
+         int deNum = RoomController.DeNum;
+         int numRoom = RoomController.Numroom;
+         int value;
+         static int guestid=0;
+        public ActionResult Checkin(FormCollection form)
+        {
+            try
+            {
+
+                
+                String roomtype = form["RoomType"];
+
+                CheckIn cm = new CheckIn();
+                cm.Id = guestid++;
+                cm.GusestName = form["GuestName"];
+                cm.Contact = Convert.ToInt64(form["contact"]);
+                cm.Address = form["address"];
+                cm.Roomtype = roomtype;
+                cm.Quantity = Convert.ToInt32(form["quantity"]);
+                cm.Staydays = Convert.ToInt32(form["Total_days"]);
+				cm.Date = DateTime.Now.Date;
+                if (cm.Roomtype.Equals("Standard"))
+                {
+                    value = 1;                    
+                }
+                if (cm.Roomtype.Equals("Delux"))
+                {
+                    value = 3;
+                }
+                if (cm.Roomtype.Equals("Premium"))
+                {
+                    value = 2;
+                }
+                int roomquantity = Convert.ToInt32(form["quantity"]);
+                switch(value)
+                {
+                    case 1: if (roomquantity <= stNum)
+                        {
+
+                            stNum = stNum - cm.Quantity;
+                            cm.Price = cm.Quantity * cm.Staydays * 1000;
+                            cm.Status = "Checkin";
+							db.CheckIns.Add(cm);
+                            Guestdetails.Add(cm);
+							db.SaveChanges();
+                            goto found;
+                        }
+                        else
+                        {
+                            Response.Write("<script>alert('" + "Rooms quantity not available" + "')</script>");
+                            goto found;
+                        }
+                        break;
+
+                    case 2: if (roomquantity <= prNum)
+                        {
+
+                            prNum = prNum - cm.Quantity;
+                            cm.Price = cm.Quantity * cm.Staydays;
+                            cm.Price = cm.Price * 2000;
+                            cm.Status = "Checkin";
+							db.CheckIns.Add(cm);
+							Guestdetails.Add(cm);
+							db.SaveChanges();
+							goto found;
+                        }
+                        else
+                        {
+                            Response.Write("<script>alert('" + "Rooms quantity not available" + "')</script>");
+                            goto found;
+                        }
+                        break;
+
+                    case 3: if (roomquantity <= deNum)
+                        {
+                            deNum = deNum - roomquantity;
+                            cm.Price = cm.Price * 3000;
+                            cm.Status = "Checkin";
+							 if (ModelState.IsValid)
+                            {
+
+
+                                try
+                                {
+                                    db.CheckIns.Add(cm);
+                                    Guestdetails.Add(cm);
+                                    db.SaveChanges();
+                                }
+                                catch (DbEntityValidationException ex)
+                                {
+                                    foreach (DbEntityValidationResult item in ex.EntityValidationErrors)
+                                    {
+                                        // Get entry
+
+                                        DbEntityEntry entry = item.Entry;
+                                        string entityTypeName = entry.Entity.GetType().Name;
+
+                                        // Display or log error messages
+
+                                        foreach (DbValidationError subItem in item.ValidationErrors)
+                                        {
+                                            string message = "Error";
+                                            Console.WriteLine(message);
+                                        }
+                                    }
+                                }
+
+                            }
+						}
+                        else
+                        {
+                            Response.Write("<script>alert('" + "Rooms quantity not available" + "')</script>");
+                        }
+                        break;
+
+                    default: Response.Write("<script>alert('" + "RoomsType not available" + "')</script>");
+                             break;
+                }
+             
+
+            }
+            catch (Exception e)
+            {
+                Response.Write("<script>alert('" + e + "')</script>");
+            }
+
+       found:    return View();
+        }
+
+
+
+
+        public ActionResult Viewchekin(CheckIn cm, FormCollection form)
+        {   
+            try
+			{
+				RoomEntities roomEntities = new RoomEntities();
+				Guestdetails = roomEntities.CheckIns.ToList();
+                TempData["Guestdetails"] = Guestdetails;
+                  
+            }
+            catch (Exception e)
+            {
+                return View();
+            }
+            return View();
+        }
+
+        public ActionResult Search(FormCollection form)
+        {
+            string name = form["search"];
+			
+			List<CheckIn> SearchList = new List<CheckIn>();
+			
+			SearchList = db.CheckIns.Where(room => room.GusestName == name).ToList();
+            TempData["Search"] = SearchList;
+            return View();
+        }
+
+
+
+        
+        [Authorize]
+        public ActionResult checkout(CheckIn cm)
+        {
+             if(cm.Roomtype.Equals("Standard"))
+             {
+				stNum = stNum + cm.Quantity;
+             }
+             if (cm.Roomtype.Equals("Premium"))
+             {
+                prNum= prNum + cm.Quantity;
+             }
+             if (cm.Roomtype.Equals("Delux"))
+             {
+                 deNum = deNum + cm.Quantity;
+             }
+             cm.Status = "Chekout";
+             cm.Date = DateTime.Now.Date;
+             Guestdetails.Remove(cm);
+             return View();
+           
+        }
+    }
+
+    
+}
---
+++ MvcApplication1/MvcApplication1/Controllers/RoomController.cs
@@ -0,0 +1,208 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class RoomController : Controller
+    {
+        //
+        // GET: /Room/
+
+
+
+        RoomEntities db = new RoomEntities();
+      
+      public static List<Room> Roomdetails = new List<Room>();
+      public static int StNum = 0;
+      public static int PrNum = 0;
+      public static int DeNum = 0;
+      public static int Numroom = 0;
+      
+            
+      /* [HttpPost]
+        public ActionResult Create()
+        {
+            try
+            {
+                RoomModel rm = new RoomModel();
+
+                rm.RoomType = collection["RoomType"];
+
+                rm.NumRooms = Convert.ToInt32(collection["NumRooms"]);
+               
+                rm.Quantity = Convert.ToInt32(collection["Quantity"]);
+                rm.Price = rm.Quantity * 1000;
+                Roomdetails.Add(rm);
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }*/
+            
+
+
+
+        [Authorize]
+        public ActionResult Create(FormCollection form)
+      {
+          
+            try
+            {   
+                String RoomType = form["RoomType"];
+                int numberofRooms= Convert.ToInt32(form["NumberofRooms"]);
+                if (RoomType.Equals("Standard"))
+                {
+                    for(int count=0; count < numberofRooms; count++){
+                    Room rm = new Room();
+                    rm.Roomtype = RoomType;
+                   
+                    rm.Id = StNum + 100;
+                    rm.Roomtype = "Standard";
+                    StNum++;
+                    Roomdetails.Add(rm);
+                    db.Rooms.Add(rm);
+                    db.SaveChanges();
+                    }
+                    
+                }
+                else   if (RoomType.Equals("Premium"))
+                {
+   
+                    for (int count = 0; count < numberofRooms; count++)
+                    {
+                        Room rm = new Room();
+                        rm.Roomtype = RoomType;
+                    
+                        rm.Id = PrNum + 500;
+                        rm.Price = 3000;
+                        PrNum++;
+                        Roomdetails.Add(rm);
+                        db.Rooms.Add(rm);
+                        db.SaveChanges();
+                    }
+                    
+                }
+                else if (RoomType.Equals("Delux"))
+                {
+                   
+                    for (int count = 0; count < numberofRooms; count++)
+                    {
+                        Room rm = new Room();
+                        rm.Roomtype = RoomType;
+                       
+                        rm.Id = DeNum + 700;
+                        rm.Price = 4000;
+                        DeNum++;
+                        Roomdetails.Add(rm);
+                        db.Rooms.Add(rm);
+                        db.SaveChanges();
+                    }
+                   
+                }
+                else
+                {
+                    for (int count = 0; count < numberofRooms; count++)
+                    {
+                        Room rm = new Room();
+                        rm.Roomtype = RoomType;
+    
+                        rm.Id = Numroom + 1000;
+                        rm.Price = 5000;
+                        Numroom++;
+                        Roomdetails.Add(rm);
+                        db.Rooms.Add(rm);
+                        db.SaveChanges();
+                    }
+                   
+                }
+
+               
+            }
+            catch (Exception e)
+            {
+                return View();    
+            }
+            return View();
+        }
+
+        
+        public ActionResult Viewdetails(Room rm)
+        {
+           
+            Roomdetails = db.Rooms.ToList();
+           TempData["Roomdetails"] = Roomdetails;
+            return View();
+        }
+
+
+        public ActionResult delete(int Id)
+        {
+            
+            Room Rd = db.Rooms.FirstOrDefault(s => s.Id == Id);
+            db.Rooms.Remove(Rd);
+            db.SaveChanges();
+           return View();
+        }
+
+
+
+
+
+  
+        public ActionResult Edit(int Id)
+        {
+            return View();
+        }
+      
+        [HttpPost]
+        [Authorize]
+        public ActionResult Edit(int Id, FormCollection form1)
+        {
+            try
+            {
+            Room rm1 = new Room();
+            string roomtype = form1["RoomType"];
+            
+            Room Rd = db.Rooms.FirstOrDefault(s => s.Id == Id);
+            
+            Rd.Roomtype = form1["RoomType"];
+           
+            if (Rd.Roomtype.Equals("Standard"))
+            {      
+                Rd.Id = StNum + 100;
+                Rd.Roomtype = "Standard";
+                StNum++;
+            }
+            if (Rd.Roomtype.Equals ("Premium"))
+            {
+                Rd.Id = PrNum + 500;
+                Rd.Roomtype = "Premium";
+                PrNum++;
+            }
+            if (Rd.Roomtype.Equals("Delux"))
+            {
+                Rd.Id = DeNum + 700;
+                Rd.Roomtype= "Delux";
+                DeNum++;
+            }
+            db.SaveChanges();
+                return RedirectToAction("Viewdetails");
+            }
+            catch
+            {
+                return View();
+            }
+        } 
+
+
+    }
+
+    
+}
